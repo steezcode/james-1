96451652110da8d86aa8d85a16041cacd8fbea23
Alberto <bodyboarddinamic@gmail.com>
2022-04-06 19:05:58 +0100

    create homescreen routing and component

--- james1/src/app/app-routing.module.ts
+++ james1/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomescreenComponent } from './pages/homescreen/homescreen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomescreenComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
